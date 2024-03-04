e59894d7581dd0f1aa8e4b108cf5b5fcbeec2788
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-04 21:23:13 +0530

    route created for creating pdf

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+const express = require('express')
+const cors = require('cors')
+const pdf = require('html-pdf')
+const pdfTemplate = require('./documents/index')
+
+const app = express()
+const port = 3000
+app.use(cors())
+
+// post request
+app.post('/create-pdf', (req, res) => {
+    const data = req.body
+    pdf.create(pdfTemplate(data), {}).toFile('receipt.pdf', (err) => {
+        if (err) {
+            res.status(500).send(Promise.reject())
+        }
+        res.status(200).send(Promise.resolve())
+    })
+})
+
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
