1f7be3fcbafa1b3fe4270df5da14ec57c261ca9d
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-05 10:41:49 +0530

    working app checkpoint

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
 const express = require('express')
 const cors = require('cors')
 const pdf = require('html-pdf')
-const pdfTemplate = require('./documents/index')
+const pdfTemplate = require('./documents')
 
 const app = express()
 const port = 3000
-app.use(cors())
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+}))
 
 // post request
 app.post('/create-pdf', (req, res) => {
     const data = req.body
-    pdf.create(pdfTemplate(data), {}).toFile('receipt.pdf', (err) => {
+    //console.log(data)
+
+    pdf.create(pdfTemplate(data)).toFile('receipt.pdf', (err) => {
         if (err) {
+            //console.log('error')
             res.status(500).send(Promise.reject())
         }
         res.status(200).send(Promise.resolve())
     })
 })
 
+// get request
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/receipt.pdf`)
+})
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
