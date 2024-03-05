0b6c9a94226ea48ec7ac1ebd7acb8648b86acca0
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-05 11:29:18 +0530

    vercel config-4

--- server/index.js
+++ server/index.js
@@ -6,16 +6,14 @@ const pdfTemplate = require('./documents')
 const app = express()
 const port = 3000
 app.use(express.json())
-// app.use(cors({
-//     origin: [
-//         'https://invoice-generator-v0.vercel.app',
-//         'https://invoice-generator-v0-backend.vercel.app',
-//         'http://localhost:5173'
-//     ],
-//     methods: ['GET', 'POST']
-// }))
-
-app.use(cors())
+//app.use(cors())
+app.use(cors({
+    origin: [
+        'https://invoice-generator-v0.vercel.app',
+        'http://localhost:5173'
+    ],
+    methods: ['GET', 'POST']
+}))
 
 // post request
 app.post('/create-pdf', (req, res) => {
